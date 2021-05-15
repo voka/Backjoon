155d5c89862513f828d4c8dbe927af7becb8ac47
voka <chdlswhd7@naver.com>
2021-05-15 12:50:41 +0900

    change contents

--- 20210515/17136_색종이붙이기.cpp
+++ 20210515/17136_색종이붙이기.cpp
@@ -4,10 +4,10 @@
 
 using namespace std;
 
-int temp[10][10] ={0,};
-int paper[5] = {5,5,5,5,5};
-int N = 10;
-int answer=123234123;
+int temp[10][10] ={0,}; // 종이판
+int color_paper[5] = {5,5,5,5,5}; // 붙일 수 있는 색종이 5장
+int N = 10;// NXN
+int answer=25; // 아무리 많이 붙여도 25장 밖에 붙일 수 없다.
 
 
 void update(int x, int y, int size,bool contents){
@@ -34,13 +34,13 @@ bool check2(){
     }
     return true;
 }
-void find_paper(int x, int y, int count){
-    if(check2()){
-        answer = min(answer,count);
+void DFS_paper(int x, int y, int count){
+    if(check2()){ // 종이에 1인 곳이 있는지 검사
+        answer = min(answer,count); // 없으면 색종이가 다 덮였으므로 현재값과 새로구한 값 중 작은값을 정답으로
         return;
     }
     bool checks = false;
-    for(x=0;x<N;++x){
+    for(x=0;x<N;++x){ // 종이판이 1인곳을 찾는다.
         for(y=0;y<N;++y){
             if(temp[x][y]){
                 checks = true;
@@ -51,17 +51,17 @@ void find_paper(int x, int y, int count){
             break;
         }
     }
-    if(count>=answer)
+    if(count>=answer)// 현재까지 구한 답이 최근에 구한 답보다 크면 미리 branch를 한다.
         return;
 
-    for(int size=5;size>0;--size){
-        if(x+size>N||y+size>N||paper[size-1] == 0) continue;
-        if(check(x,y,size)){
-            update(x,y,size,0);
-            --paper[size-1];
-            find_paper(x,y,count+1);
-            update(x,y,size,1);
-            ++paper[size-1];
+    for(int size=5;size>0;--size){// 5,4,3,2,1의 사이즈 순서로 차례로 색종이를 붙인다.
+        if(x+size>N||y+size>N||color_paper[size-1] == 0) continue; // 색종이를 붙일 수 없거나, 붙일 색종이가 없는경우 제외한다.
+        if(check(x,y,size)){ // x,y 부터 시작해 크기가 size인 색종이를 붙일 수 있는지 검사 
+            update(x,y,size,0); // size X size만큼을 0으로 바꾼다
+            --color_paper[size-1];//color color_paper 개수 감소
+            DFS_paper(x,y,count+1);
+            update(x,y,size,1);// 원상복구
+            ++color_paper[size-1];// 원상복구
         }
     }
 }
@@ -72,8 +72,8 @@ int main(void){
             cin>>temp[i][j];
         }
     }
-    find_paper(0,0,0);
-    if(answer != 123234123) cout<<answer<<"\n";
+    DFS_paper(0,0,0);
+    if(answer != 25) cout<<answer<<"\n";
     else cout<<-1<<"\n";
     return 0;
 }
