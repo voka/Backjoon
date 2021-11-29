12b04d8c410981ab14cdc4539072527b387d9e44
voka <chdlswhd7@naver.com>
2021-11-29 11:07:25 +0900

    추가 note"

--- 20211129/pro_hash_table3.py
+++ 20211129/pro_hash_table3.py
@@ -11,11 +11,11 @@ def solution(genres, plays):
             musicpopulur[genres[i]] += plays[i]
         else:
             musicpopulur[genres[i]] = plays[i]
-    ss = sorted(musicpopulur.items(), key = lambda item : item[1] ,reverse=True )
+    ss = sorted(musicpopulur.items(), key = lambda item : item[1] ,reverse=True ) # 딕셔너리 정렬해서 튜플리스트로 만드는 것도 외워둘 것 !
     
     answer = []
     for i in ss:
-        mymusic[i[0]].sort(key=lambda x : (x[0],-x[1]), reverse=True)
+        mymusic[i[0]].sort(key=lambda x : (x[0],-x[1]), reverse=True) # 요런식으로 정렬함수 사용하면 n 차원도 정렬할 수 있을거 같다. 유용 !!!
         answer.append(mymusic[i[0]][0][1])
         if(len(mymusic[i[0]]) > 1) : answer.append(mymusic[i[0]][1][1])
         
