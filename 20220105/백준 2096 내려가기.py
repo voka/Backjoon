d30b7313505b5a1d692dc6333f386d6093cacd93
voka <chdlswhd7@naver.com>
2022-01-05 22:59:29 +0900

    yap

---
+++ 20220105/백준 2096 내려가기.py
@@ -0,0 +1,25 @@
+from sys import stdin 
+n = int(stdin.readline())
+
+dp_max = [0]*(3)
+temp_max = [0]*(3)
+dp_min = [0]*(3)
+temp_min = [0]*(3)
+
+for i in range(n):
+    f,s,t = list(map(int,stdin.readline().split()))
+    for j in range(3):
+        if(j == 0):
+            temp_max[j] = f + max(dp_max[j],dp_max[j+1])
+            temp_min[j] = f + min(dp_min[j],dp_min[j+1])
+
+        elif(j == 1):
+            temp_max[j] = s + max(dp_max[j],dp_max[j-1],dp_max[j+1])
+            temp_min[j] = s + min(dp_min[j],dp_min[j-1],dp_min[j+1])
+        else:
+            temp_max[j] = t + max(dp_max[j],dp_max[j-1])
+            temp_min[j] = t + min(dp_min[j],dp_min[j-1])
+    dp_max = temp_max[:]
+    dp_min = temp_min[:]    
+print(max(dp_max), end=" ")
+print(min(dp_min))
