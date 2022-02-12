3a6515f56a1a16686e4e8e2105284c735c4b98ac
voka <chdlswhd7@naver.com>
2022-02-12 23:19:24 +0900

    내 생각 추가

--- 20220212/백준 17071 숨바꼭질5.py
+++ 20220212/백준 17071 숨바꼭질5.py
@@ -10,6 +10,19 @@ while myq:
         print(-1)
         break
     f = t%2
+    """ 
+    f -> 홀수인지 짝수인지 구분하는 flag
+    if check[f][cur_K] != -1: <- 요놈의 의미는
+    1. 현재 시간이 홀수일때 cur_K인 지점을 홀수 시간대에 방문했다.
+    2. 현재 시간이 짝수일때 cur_K인 지점을 짝수 시간대에 방문했다. 
+    위 두 가지 경우 그 지점을 앞뒤(cur+1 -> cur-1)로 이동하면서 해당 지점을 지켰다고 가정하고 
+    K가 이동해서 cur_K가 된 시점의 시간인 t를 출력하겠다는 뜻이다. 
+    
+    cur == cur_K인 경우도 아래의 경우에 포함되는데,
+    cur == cur_K라면
+    현재 시간인 t 시점에 check[f][cur] = check[f][cur_K] = 1 이고 
+    check[f][cur_K] != -1이라 t를 출력하게 된다. 
+    """
     if check[f][cur_K] != -1:
         print(t)
         break
