80d1d516b04d28d5abbeee7c606f09463da2fabd
voka <chdlswhd7@naver.com>
2023-05-04 00:21:15 +0900

    풀었다 ?

--- 20230503/12764 싸지방에 간 준하/Main.java
+++ 20230503/12764 싸지방에 간 준하/Main.java
@@ -1,7 +1,16 @@
 import java.io.*;
 import java.util.*;
 
+// 시작시간 비교용 우선순위 큐 1개
+// 자리별 끝나는 시간 비교용 우선순위 큐 1개
+// 인덱스 비교용 우선순위 큐 1개 (빈 자리중 가장 작은 인덱스를 가진 자리를 먼저 사용해야함)
+// 사용횟수 기록용 배열은 100001 크기로 미리 선언 
+// i 번째 시작시간과 1 ~ i-1 번째 중 가장 빨리 종료 시키는 시간 비교 
+// 출력은 StringBuilder를 이용해 빠르게 하기
+//
+
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static class Comp implements Comparable<Comp> {
         int start;
         int end;
@@ -13,11 +22,7 @@ public class Main {
 
         @Override
         public int compareTo(Comp target) {
-            if(this.start!=target.start){
-                return this.start - target.start;
-            }
-            else
-                return this.end - target.end;
+            return this.start - target.start;
         }
 
         @Override
@@ -40,43 +45,33 @@ public class Main {
             pq.offer(new Comp(p,q));
         }
         PriorityQueue<Comp> seat = new PriorityQueue<>();
-        seat.offer(new Comp(0, 0)); // idx, end_time
-        List<Integer> answer = new ArrayList<>(); // count
-        answer.add(0);
+        PriorityQueue<Integer> nextSeat = new PriorityQueue<>();
+        nextSeat.add(0); // idx
+        int [] answer = new int[100001]; // count
         while(!pq.isEmpty()){
-            List<Integer> tt = new ArrayList<>();
             Comp c = pq.poll();
-            while(true){
-                Comp temp = seat.peek();
-                if(seat.isEmpty() || temp.start > c.start ){
-                    for(int t : tt){
-                        seat.offer(new Comp(0,t));
-                    }
-                    break;
-                }
-                seat.remove();
-                tt.add(temp.end);
+            while(!seat.isEmpty() && seat.peek().start < c.start){ // 초기화
+                nextSeat.add(seat.poll().end);
             }
-            Comp tmp = seat.peek();
-            if(c.start < tmp.start){// 새로운 자리 만들기
+            if(nextSeat.isEmpty()){ // 새로운 자리 만들기
                 int size = seat.size();
                 seat.offer(new Comp(c.end,size));
-                answer.add(0);
-                answer.set(size,answer.get(size) + 1);
+                answer[size]++;
             }
             else{// 기존 자리 바꾸기
-                seat.remove();
-                int idx = tmp.end;
-                answer.set(idx, answer.get(idx) + 1);
+                int idx = nextSeat.poll();
+                answer[idx]++;
                 seat.offer(new Comp(c.end,idx));
-
             }
-            //System.out.println(c + " " + tmp + " " + seat.peek());
+            //System.out.println(c + " " + seat.peek() + " " + nextSeat.size());
         }
-        System.out.println(seat.size());
-        for(int i : answer){
-            System.out.print(i + " ");
+
+        int count = seat.size() + nextSeat.size();
+        sb.append(count+"\n");
+        for(int i = 0; i<count;++i){
+            sb.append(answer[i] + " ");
         }
+        System.out.println(sb.toString());
     }
 
 }
